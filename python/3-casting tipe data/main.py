1a283b165bafab3b16041fb1d4734890f492c0c2
suspendisse02 <suspendisse02@gmail.com>
2022-03-18 08:45:25 +0800

    Created Python3.x_Dasar_Programming-master

--- python/3-casting tipe data/main.py
+++
@@ -1,8 +0,0 @@
-# kita belaja casting
-# merubah dari satu tipe ke tipe lain
-# tipe data = int, float, str, bool
-data_int = 9
-print('data =', data_int, 'type =', type(data_int))
-
-data_float = float(data_int)
-print('data =', data_float, 'type =', type(data_float))
